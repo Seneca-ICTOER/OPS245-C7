2fe32f1bbb8ff9d9bbf0978be29e466a7721426a
Nathan <65503430+NateKenopic@users.noreply.github.com>
2023-06-15 15:33:54 -0400

    Script Samples for Weekly Schedule Weeks 1-8

---
+++ static/files/if1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+Purpose: if1.py
+This is a simple python script demonstrating if statements with ints
+
+Author: Peter Callaghan
+Date: 05 Feb. '21
+
+'''
+
+val1='teal'
+val2='teamwork'
+
+if val1 < val2:
+  print("I think ", val1," is less than ",val2)
+  print("This is another line inside the if statement")
+print("this code is not inside the if statement")
+print("Because it is not indented")
